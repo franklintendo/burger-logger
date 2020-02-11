8a6c5ebe723b9e3464c4a10979fc9473fb6e6341
Frank Moran <info@francisfrankmoran.com>
2020-02-10 20:57:57 -0500

    seed sql

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+const mysql = require("mysql");
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 7080;
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
